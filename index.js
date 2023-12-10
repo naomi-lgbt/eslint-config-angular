be9446b6a7e49b74996dda2088bc7e8828b8652b
Naomi Carrigan <nhcarrigan@gmail.com>
2023-12-10 15:37:53 -0800

    fix: override import and instantiaton rules in test files (#4)

--- index.js
+++ index.js
@@ -33,6 +33,39 @@ module.exports = {
       },
     },
     {
+      files: ["*.spec.ts", "**/test.ts"],
+      parserOptions: {
+        project: ["tsconfig.json"],
+        createDefaultProgram: true,
+      },
+      extends: [
+        "@nhcarrigan/eslint-config",
+        "plugin:@angular-eslint/recommended",
+        "plugin:@angular-eslint/template/process-inline-templates",
+      ],
+      rules: {
+        "import/no-unresolved": "off",
+        "init-declarations": "off",
+        "@angular-eslint/no-empty-lifecycle-method": "off",
+        "@angular-eslint/directive-selector": [
+          "error",
+          {
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+          },
+        ],
+        "@angular-eslint/component-selector": [
+          "error",
+          {
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+          },
+        ],
+      },
+    },
+    {
       files: ["*.html"],
       extends: ["plugin:@angular-eslint/template/recommended"],
       rules: {},
