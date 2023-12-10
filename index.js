ae43cbef85ff10ee228233402dab4671eb2ceff4
Naomi Carrigan <nhcarrigan@gmail.com>
2023-12-10 15:14:22 -0800

    feat: config is tested and working locally

--- index.js
+++ index.js
@@ -1,82 +1,41 @@
 module.exports = {
-  env: {
-    es2020: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "plugin:jsdoc/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 11,
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint", "jsdoc", "import", "no-only-tests"],
-  rules: {
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double", { allowTemplateLiterals: true }],
-    semi: ["error", "always"],
-    "prefer-const": "warn",
-    eqeqeq: ["error", "always"],
-    curly: ["error"],
-    "require-atomic-updates": ["warn"],
-    "no-var": ["error"],
-    camelcase: ["error"],
-    "comma-dangle": ["error", "never"],
-    "init-declarations": ["error", "always"],
-    "require-await": ["warn"],
-    "no-param-reassign": ["error"],
-    "jsdoc/require-jsdoc": [
-      "warn",
-      {
-        require: {
-          ArrowFunctionExpression: true,
-          ClassDeclaration: true,
-          ClassExpression: true,
-          FunctionDeclaration: true,
-          FunctionExpression: true,
-          MethodDefinition: true,
-        },
-        publicOnly: true,
+  root: true,
+  overrides: [
+    {
+      files: ["*.ts"],
+      parserOptions: {
+        project: ["tsconfig.json"],
+        createDefaultProgram: true,
       },
-    ],
-    "jsdoc/require-description-complete-sentence": "warn",
-    "import/first": "warn",
-    "import/exports-last": "warn",
-    "import/newline-after-import": "warn",
-    "import/order": [
-      "warn",
-      {
-        groups: [
-          "builtin",
-          "external",
-          "internal",
-          "parent",
-          "sibling",
-          "index",
-          "object",
-          "type",
-          "unknown",
+      extends: [
+        "@nhcarrigan/eslint-config",
+        "plugin:@angular-eslint/recommended",
+        "plugin:@angular-eslint/template/process-inline-templates",
+      ],
+      rules: {
+        "@angular-eslint/no-empty-lifecycle-method": "off",
+        "@angular-eslint/directive-selector": [
+          "error",
+          {
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+          },
         ],
-        "newlines-between": "always",
-        alphabetize: {
-          order: "asc",
-          caseInsensitive: true,
-        },
+        "@angular-eslint/component-selector": [
+          "error",
+          {
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+          },
+        ],
       },
-    ],
-    "no-only-tests/no-only-tests": [
-      "warn",
-      {
-        block: ["test", "suite", "assert"],
-        focus: ["only", "skip"],
-      },
-    ],
-    "no-console": "warn",
-  },
+    },
+    {
+      files: ["*.html"],
+      extends: ["plugin:@angular-eslint/template/recommended"],
+      rules: {},
+    },
+  ],
 };
